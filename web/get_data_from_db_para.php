8cff186c979d46546321e5846d28f9d267dbdfdd
Zhengchun Liu <liuzhengchun@gmail.com>
2015-08-28 13:40:55 +0200

    add web

---
+++ web/get_data_from_db_para.php
@@ -0,0 +1,24 @@
+<?php
+if (isset($argv)) {
+    $latest_min = $argv[1];
+}
+else
+{
+    $latest_min = $_GET["latest_min"];
+}
+$con = mysql_connect("localhost","root","lock");
+
+if (!$con) {
+  die('Could not connect: ' . mysql_error());
+}
+
+mysql_select_db("home_status", $con);
+
+$result = mysql_query("SELECT * FROM humidity_and_temperature WHERE humidity_and_temperature.date_time > DATE_SUB(now(), INTERVAL ".$latest_min." MINUTE);");
+
+while($row = mysql_fetch_array($result)) {
+    echo $row['date_time'] . "\t" . $row['humidity']. "\t" . $row['temperature']. "\n";
+}
+
+mysql_close($con);
+?>
